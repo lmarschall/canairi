b1088eb9d2bdc4f7b9f6abc42884ac6283cb7c96
lmarschall <lmarschall.com@gmail.com>
2021-08-02 17:25:36 +0200

    updated get measurements api endpoint

--- server/app/views.py
+++ server/app/views.py
@@ -12,24 +12,17 @@ def get_measurements(request):
 
     # http://127.0.0.1:8000/api/rooms?size=1&check_in=08.08.2020&check_out=12.08.2020
 
-    # try:
-    #     # try to get the filter parameters from the request
-    #     size = request.query_params['size']
-    #     check_in = request.query_params['check_in']
-    #     check_out = request.query_params['check_out']
+    try:
+        # try to get the filter parameters from the request
+        start_date = request.query_params["start_date"]
+        end_date = request.query_params["end_date"]
 
-    #     date_one = datetime.date(int(check_in.split('.')[2]), int(check_in.split('.')[1]), int(check_in.split('.')[0]))
-    #     date_two = datetime.date(int(check_out.split('.')[2]), int(check_out.split('.')[1]), int(check_out.split('.')[0]))
+        # filter out avaible rooms
+        measurements = Measurement.objects.filter(check_out__gte=start_date).filter(check_in__lte=end_date).all().order_by("time")
 
-    #     # filter out avaible rooms
-    #     reserved_room_ids = Reservation.objects.filter(check_out__gte=date_one).filter(check_in__lte=date_two).values_list('id', flat=True)
-    #     rooms = Room.objects.filter(size__gte=size).exclude(id__in = reserved_room_ids).all()
-
-    # except:
-    #     # get all rooms if no valid paramters were given
-    #     rooms = Room.objects.all()
-
-    measurements = Measurement.objects.all()
+    except:
+        # get all rooms if no valid paramters were given
+        measurements = Measurement.objects.all().order_by("-time")[:60]
 
     # parse the filtered rooms to json
     json_measurements = measurements.values("id", "time", "temperature", "pressure", "humidity", "gas_resistance", "air_quality")
